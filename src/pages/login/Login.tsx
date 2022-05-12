9a154ea9d5183ec5d51419e27a05731afce0805d
Rick Sousa <rickssti@outlook.com>
2022-05-12 13:19:11 -0300

    ✨ feat: add Register page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import { Button, Input } from "../../components";
 import { GoogleSvg, FacebookSvg } from "../../assets";
+import { fetchSign } from "../../utils/api";
 
 export default function Login() {
   const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  async function fetchLogin() {
+    const data = await fetchSign(email, password, "login");
+    if (data.message !== "success") {
+      setIsValid(false);
+    }
+  }
+
   return (
     <div className="bg-back1 h-screen flex flex-col">
       <div className="flex justify-center items-center h-1/3">
@@ -12,11 +22,20 @@ export default function Login() {
       </div>
 
       <div className="flex flex-1 flex-col bg-back3 px-8">
+        <span className="text-red-600 text-center mt-2 h-6">
+          {!isValid && "Invalid user or password"}
+        </span>
+
         <div className="mb-6">
-          <Input label="Email" setInput={setEmail} hidden />
-          <Input label="Password" setInput={() => null} />
+          <Input label="Email" setInput={setEmail} />
+          <Input label="Password" setInput={setPassword} hidden />
         </div>
-        <Button title="Entrar" onPress={() => null} />
+
+        <Button
+          title="Entrar"
+          onPress={() => fetchLogin()}
+          isDisabled={!email && !password}
+        />
         <span className="text-sm text-font mt-10 mb-4 text-center">
           Or continue with
         </span>
@@ -24,12 +43,12 @@ export default function Login() {
           <Button
             title="Google"
             Icon={GoogleSvg}
-            onPress={() => console.log(email)}
+            onPress={() => console.log("Google")}
           />
           <Button
             title="Facebook"
             Icon={FacebookSvg}
-            onPress={() => console.log(email)}
+            onPress={() => console.log("Facebook")}
           />
         </div>
         <span className="text-sm text-font mt-10 mb-4 text-center">
---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Button, Input } from "../../components";
+import { fetchSign } from "../../utils/api";
+
+export default function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function fetchRegister() {
+    const data = await fetchSign(email, password, "registration", name);
+    if (data.message !== "success") {
+      setMessage(data.message);
+    }
+  }
+
+  return (
+    <div className="bg-back1 h-screen flex flex-col">
+      <div className="flex justify-center items-center h-1/3">
+        <span className="text-white text-3xl font-extrabold">Register</span>
+      </div>
+
+      <div className="flex flex-1 flex-col bg-back3 px-8">
+        <span className="text-red-600 text-center mt-2 h-6">{message}</span>
+
+        <div className="mb-6">
+          <Input label="Name" setInput={setName} />
+          <Input label="Email" setInput={setEmail} />
+          <Input label="Password" setInput={setPassword} hidden />
+        </div>
+        <Button
+          title="Register"
+          onPress={() => fetchRegister()}
+          isDisabled={!name && !email && !password}
+        />
+      </div>
+    </div>
+  );
+}
