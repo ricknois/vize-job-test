f9e1e8708fa2996a9ab0ebf9ecd4eb8cddf2e01f
Rick Sousa <rickssti@outlook.com>
2022-05-13 14:52:25 -0300

    ✨ feat: implements users fetch

--- src/utils/api/apiInterfaces.ts
+++ src/utils/api/apiInterfaces.ts
@@ -9,3 +9,19 @@ export interface SignInUpTypes {
   message: string;
   data: Data | null;
 }
+
+export interface UsersData {
+  id: number;
+  name: string;
+  email: string;
+  profilepicture: string;
+  location: string;
+  createdat: Date;
+}
+export interface UsersInterface {
+  page: number;
+  per_page: number;
+  totalrecord: number;
+  total_pages: number;
+  data: UsersData[];
+}
--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,9 +1,10 @@
-import { SignInUpTypes } from "./apiInterfaces";
+import { SignInUpTypes, UsersInterface } from "./apiInterfaces";
 
 const baseUrl = "https://devfront.vize.solutions/api/";
 const apiList: Record<string, string> = {
   login: "/authaccount/login",
   registration: "/authaccount/registration",
+  users: "/users?page=",
 };
 
 export async function fetchSign(
@@ -13,13 +14,31 @@ export async function fetchSign(
   name?: string
 ): Promise<SignInUpTypes> {
   const body = { email, password, name };
-  console.log(body);
   const url = `${baseUrl}${apiList[type]}`;
-  const data = fetch(url, {
+  const data: Promise<SignInUpTypes> = fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   }).then((response) => response.json());
 
   return data;
+}
+
+export async function fetchUsers(
+  token: string,
+  type: string,
+  page: number
+): Promise<UsersInterface> {
+  const url = `${baseUrl}${apiList[type]}${page}`;
+  return <Promise<UsersInterface>>fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw new Error();
+    }
+    return response.json();
+  });
 }
